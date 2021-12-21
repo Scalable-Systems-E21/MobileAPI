30f1bf56bfa2d18660259eeeae666af3c75b0a6e
Julie Dittmann Weimar Andersen <32199781+DittmannWeimar@users.noreply.github.com>
2021-12-21 15:08:17 +0100

    formatting fixes

--- src/kafka.ts
+++ src/kafka.ts
@@ -12,100 +12,104 @@ const hb = hbase({
     port: process.env.HBASE_PORT
 })
 
-export async function getResult (bounds: GeoBounds) {
-                    // tslint:disable-next-line:no-console
+export async function getResult(bounds: GeoBounds) {
+    // tslint:disable-next-line:no-console
     console.log('A')
     try {
         const tname = process.env.HBASE_TABLE
-    const table = hb.table(tname)
-                    // tslint:disable-next-line:no-console
-                    console.log('B')
+        const table = hb.table(tname)
+        // tslint:disable-next-line:no-console
+        console.log('B')
 
-    table.exists((error: any, success: boolean) => {
-        if (success) {
-                                // tslint:disable-next-line:no-console
-    console.log('C')
-            table.scan({
-                maxVersions: 1, // Might need to change if we want to query back in time.
-                filter: filter(bounds, 0, Date.now())
-            }, (err: any, rows: any) => {
-                                    // tslint:disable-next-line:no-console
-    console.log('D')
-                if (err !== undefined) {
-                                        // tslint:disable-next-line:no-console
-    console.log('E')
+        table.exists((error: any, success: boolean) => {
+            // tslint:disable-next-line:no-console
+            console.log('BB')
+            if (success) {
+                // tslint:disable-next-line:no-console
+                console.log('C')
+                table.scan({
+                    maxVersions: 1, // Might need to change if we want to query back in time.
+                    filter: filter(bounds, 0, Date.now())
+                }, (err: any, rows: any) => {
                     // tslint:disable-next-line:no-console
-                    console.log(err)
-                }else{
-                                        // tslint:disable-next-line:no-console
-    console.log('F')
-                    // tslint:disable-next-line:no-console
-                    console.log(rows)
-                    return rows
-                }
-            })
-        }
-        return null
-    })
-}catch (e) {
-    return e
-}
+                    console.log('D')
+                    if (err !== undefined) {
+                        // tslint:disable-next-line:no-console
+                        console.log('E')
+                        // tslint:disable-next-line:no-console
+                        console.log(err)
+                    } else {
+                        // tslint:disable-next-line:no-console
+                        console.log('F')
+                        // tslint:disable-next-line:no-console
+                        console.log(rows)
+                        return rows
+                    }
+                })
+            }
+            return null
+        })
+    } catch (e) {
+        return e
+    }
 }
 
-function filter (bounds: GeoBounds, timestampMin: number, timestampMax: number) {
-    return {'op': 'MUST_PASS_ALL', 'type': 'FILTER_LIST', 'filters': [
-        latMinFilter(bounds.minLat),
-        latMaxFilter(bounds.maxLat),
-        lonMinFilter(bounds.minLon),
-        lonMaxFilter(bounds.maxLon),
-        //timestampMinFilter(timestampMin),
-        //timestampMaxFilter(timestampMax) // Unsure how to implement
-    ]}
+function filter(bounds: GeoBounds, timestampMin: number, timestampMax: number) {
+    return {
+        'op': 'MUST_PASS_ALL', 'type': 'FILTER_LIST', 'filters': [
+            latMinFilter(bounds.minLat),
+            latMaxFilter(bounds.maxLat),
+            lonMinFilter(bounds.minLon),
+            lonMaxFilter(bounds.maxLon),
+            //timestampMinFilter(timestampMin),
+            //timestampMaxFilter(timestampMax) // Unsure how to implement
+        ]
+    }
 }
 
 // This could be generified a lot but shut up. Might actually do it if it doesn't quickly work out and I have to tweak a bunch.
-function latMinFilter (latMin: number) {
-    return {'op': 'GREATER', 'type': 'SingleColumnValueFilter', 'family': 'shape', 'qualifier': 'lat', 'comparator': { 'value': latMin, 'type': 'BinaryComparator' }, 'filterIfColumnMissing': 'true' }
+function latMinFilter(latMin: number) {
+    return { 'op': 'GREATER', 'type': 'SingleColumnValueFilter', 'family': 'shape', 'qualifier': 'lat', 'comparator': { 'value': latMin, 'type': 'BinaryComparator' }, 'filterIfColumnMissing': 'true' }
 }
 
-function latMaxFilter (latMax: number) {
-    return {'op': 'LESS', 'type': 'SingleColumnValueFilter', 'family': 'shape', 'qualifier': 'lat', 'comparator': { 'value': latMax, 'type': 'BinaryComparator' }, 'filterIfColumnMissing': 'true' }
+function latMaxFilter(latMax: number) {
+    return { 'op': 'LESS', 'type': 'SingleColumnValueFilter', 'family': 'shape', 'qualifier': 'lat', 'comparator': { 'value': latMax, 'type': 'BinaryComparator' }, 'filterIfColumnMissing': 'true' }
 }
 
-function lonMinFilter (lonMin: number) {
-    return {'op': 'GREATER', 'type': 'SingleColumnValueFilter', 'family': 'shape', 'qualifier': 'long', 'comparator': { 'value': lonMin, 'type': 'BinaryComparator' }, 'filterIfColumnMissing': 'true' }
+function lonMinFilter(lonMin: number) {
+    return { 'op': 'GREATER', 'type': 'SingleColumnValueFilter', 'family': 'shape', 'qualifier': 'long', 'comparator': { 'value': lonMin, 'type': 'BinaryComparator' }, 'filterIfColumnMissing': 'true' }
 }
 
-function lonMaxFilter (lonMax: number) {
-    return {'op': 'LESS', 'type': 'SingleColumnValueFilter', 'family': 'shape', 'qualifier': 'long', 'comparator': { 'value': lonMax, 'type': 'BinaryComparator' }, 'filterIfColumnMissing': 'true' }
+function lonMaxFilter(lonMax: number) {
+    return { 'op': 'LESS', 'type': 'SingleColumnValueFilter', 'family': 'shape', 'qualifier': 'long', 'comparator': { 'value': lonMax, 'type': 'BinaryComparator' }, 'filterIfColumnMissing': 'true' }
 }
- 
-function timestampMinFilter (min: number) {
+
+function timestampMinFilter(min: number) {
     return {}
 }
 
-function timestampMaxFilter (max: number) {
+function timestampMaxFilter(max: number) {
     return {}
 }
 
-function geoBoundsToGeoJson (bounds: GeoBounds) {
-    const geojson = { 
+function geoBoundsToGeoJson(bounds: GeoBounds) {
+    const geojson = {
         "type": "FeatureCollection",
         "features": [
-        { 
-            "type": "Feature",
-            "properties": {},
-            "geometry": {
-                "type": "Polygon",
-                "coordinates": [[[bounds.minLon, bounds.maxLat], [bounds.maxLon, bounds.maxLat], [bounds.maxLon, bounds.minLat], [bounds.minLon, bounds.minLat], [bounds.minLon, bounds.maxLat]]]
-            }
-        }]
+            {
+                "type": "Feature",
+                "properties": {},
+                "geometry": {
+                    "type": "Polygon",
+                    "coordinates": [[[bounds.minLon, bounds.maxLat], [bounds.maxLon, bounds.maxLat], [bounds.maxLon, bounds.minLat], [bounds.minLon, bounds.minLat], [bounds.minLon, bounds.maxLat]]]
+                }
+            }]
     }
     return geojson
 }
 
 // Returns request UUID.
-export async function sendRequest (bounds: GeoBounds) {
+export async function sendRequest(bounds: GeoBounds) {
 
     const producer = kafka.producer({
         maxInFlightRequests: 1,
@@ -120,7 +124,7 @@ export async function sendRequest (bounds: GeoBounds) {
         await producer.send({
             topic: process.env.KAFKA_TOPIC,
             messages: [
-                {key: process.env.KAFKA_KEY, value: JSON.stringify(request)},
+                { key: process.env.KAFKA_KEY, value: JSON.stringify(request) },
             ]
         })
 
