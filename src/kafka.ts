e9b9847df091e69d85e02ffbf589bfa322a5a255
Julie Dittmann Weimar Andersen <32199781+DittmannWeimar@users.noreply.github.com>
2021-12-21 23:01:12 +0100

    returns working shapes now

--- src/kafka.ts
+++ src/kafka.ts
@@ -15,6 +15,7 @@ const hb = hbase({
 export async function getResult(bounds: GeoBounds) {
     const tname = process.env.HBASE_TABLE
     const table = hb.table(tname)
+    const maxShapes = 50;
 
     return new Promise(resolve => {
 
@@ -29,23 +30,19 @@ export async function getResult(bounds: GeoBounds) {
                             // tslint:disable-next-line:no-console
                             console.log(err)
                         } else {
-
                             var shapes = []
-                            var toPrint = 100
-                            for (let i: number; i < rows.length; i++) {
-                                let row = rows[i]
-                                if (row['column'] === 'shape.geojson') {
-                                    shapes.push(JSON.parse(row['$']))
+                            for (var row of rows) {
+                                // tslint:disable-next-line:no-console
+                                if (row.column === 'shape:geojson') {
+                                    var geometry = JSON.parse(row['$'])
+                                    if (shapes.length < maxShapes) {
+                                        shapes.push(feature(JSON.parse(row['$'])))
+                                    }
                                 }
-                                if (toPrint > 0) {
-                                    // tslint:disable-next-line:no-console
-                                    console.log(row)
-                                }
-                                toPrint--;
                             }
                             if (shapes.length > 0) {
                                 // tslint:disable-next-line:no-console
-                                console.log('Shapes! :D')
+                                console.log(shapes.length.toString() + ' shapes! :D')
                                 resolve(featureCollection(shapes))
                             } else {
                                 // tslint:disable-next-line:no-console
@@ -58,7 +55,6 @@ export async function getResult(bounds: GeoBounds) {
                     // tslint:disable-next-line:no-console
                     console.log(error)
                 }
-                resolve (null)
             })
         } catch (error) {
             // tslint:disable-next-line:no-console
@@ -67,102 +63,98 @@ export async function getResult(bounds: GeoBounds) {
     })
 }
 
-function polygon(coordinates) {
-            return {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Polygon",
-                    "coordinates": [
-                        coordinates
-                    ]
-                },
-            }
-        }
+function feature(geometry) {
+    return {
+        "type": "Feature",
+        "properties": {},
+        "geometry": geometry
+    }
+}
 
 function featureCollection(features) {
-            return {
-                "type": "FeatureCollection",
-                "features": features
-            }
-        }
+    return {
+        "type": "FeatureCollection",
+        "features": features
+    }
+}
 
 function filter(bounds: GeoBounds, timestampMin: number, timestampMax: number) {
-            return {
-                'op': 'MUST_PASS_ALL', 'type': 'FilterList', 'filters': [
-                    latMinFilter(bounds.minLat),
-                    latMaxFilter(bounds.maxLat),
-                    lonMinFilter(bounds.minLon),
-                    lonMaxFilter(bounds.maxLon),
-                    //timestampMinFilter(timestampMin),
-                    //timestampMaxFilter(timestampMax) // Unsure how to implement
-                ]
-            }
-        }
+    return {
+        'op': 'MUST_PASS_ALL', 'type': 'FilterList', 'filters': [
+            latMinFilter(bounds.minLat),
+            latMaxFilter(bounds.maxLat),
+            lonMinFilter(bounds.minLon),
+            lonMaxFilter(bounds.maxLon),
+            //timestampMinFilter(timestampMin),
+            //timestampMaxFilter(timestampMax) // Unsure how to implement
+        ]
+    }
+}
 
 // This could be generified a lot but shut up. Might actually do it if it doesn't quickly work out and I have to tweak a bunch.
 function latMinFilter(latMin: number) {
-            return { 'op': 'GREATER', 'type': 'SingleColumnValueFilter', 'family': 'shape', 'qualifier': 'lat', 'comparator': { 'value': latMin.toString(), 'type': 'BinaryComparator' }, 'filterIfColumnMissing': 'true' }
-        }
+    return { 'op': 'GREATER', 'type': 'SingleColumnValueFilter', 'family': 'shape', 'qualifier': 'lat', 'comparator': { 'value': latMin.toString(), 'type': 'BinaryComparator' }, 'filterIfColumnMissing': 'true' }
+}
 
 function latMaxFilter(latMax: number) {
-            return { 'op': 'LESS', 'type': 'SingleColumnValueFilter', 'family': 'shape', 'qualifier': 'lat', 'comparator': { 'value': latMax.toString(), 'type': 'BinaryComparator' }, 'filterIfColumnMissing': 'true' }
-        }
+    return { 'op': 'LESS', 'type': 'SingleColumnValueFilter', 'family': 'shape', 'qualifier': 'lat', 'comparator': { 'value': latMax.toString(), 'type': 'BinaryComparator' }, 'filterIfColumnMissing': 'true' }
+}
 
 function lonMinFilter(lonMin: number) {
-            return { 'op': 'GREATER', 'type': 'SingleColumnValueFilter', 'family': 'shape', 'qualifier': 'long', 'comparator': { 'value': lonMin.toString(), 'type': 'BinaryComparator' }, 'filterIfColumnMissing': 'true' }
-        }
+    return { 'op': 'GREATER', 'type': 'SingleColumnValueFilter', 'family': 'shape', 'qualifier': 'long', 'comparator': { 'value': lonMin.toString(), 'type': 'BinaryComparator' }, 'filterIfColumnMissing': 'true' }
+}
 
 function lonMaxFilter(lonMax: number) {
-            return { 'op': 'LESS', 'type': 'SingleColumnValueFilter', 'family': 'shape', 'qualifier': 'long', 'comparator': { 'value': lonMax.toString(), 'type': 'BinaryComparator' }, 'filterIfColumnMissing': 'true' }
-        }
+    return { 'op': 'LESS', 'type': 'SingleColumnValueFilter', 'family': 'shape', 'qualifier': 'long', 'comparator': { 'value': lonMax.toString(), 'type': 'BinaryComparator' }, 'filterIfColumnMissing': 'true' }
+}
 
 function timestampMinFilter(min: number) {
-            return {}
-        }
+    return {}
+}
 
 function timestampMaxFilter(max: number) {
-            return {}
-        }
+    return {}
+}
 
 function geoBoundsToGeoJson(bounds: GeoBounds) {
-            const geojson = {
-                "type": "FeatureCollection",
-                "features": [
-                    {
-                        "type": "Feature",
-                        "properties": {},
-                        "geometry": {
-                            "type": "Polygon",
-                            "coordinates": [[[bounds.minLon, bounds.maxLat], [bounds.maxLon, bounds.maxLat], [bounds.maxLon, bounds.minLat], [bounds.minLon, bounds.minLat], [bounds.minLon, bounds.maxLat]]]
-                        }
-                    }]
-            }
-            return geojson
-        }
+    const geojson = {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "properties": {},
+                "geometry": {
+                    "type": "Polygon",
+                    "coordinates": [[[bounds.minLon, bounds.maxLat], [bounds.maxLon, bounds.maxLat], [bounds.maxLon, bounds.minLat], [bounds.minLon, bounds.minLat], [bounds.minLon, bounds.maxLat]]]
+                }
+            }]
+    }
+    return geojson
+}
 
 // Returns request UUID.
 export async function sendRequest(bounds: GeoBounds) {
 
-        const producer = kafka.producer({
-            maxInFlightRequests: 1,
-            idempotent: true,
-            transactionalId: process.env.KAFKA_PRODUCER_ID
+    const producer = kafka.producer({
+        maxInFlightRequests: 1,
+        idempotent: true,
+        transactionalId: process.env.KAFKA_PRODUCER_ID
+    })
+    await producer.connect()
+
+    let request = geoBoundsToGeoJson(bounds)
+
+    try {
+        await producer.send({
+            topic: process.env.KAFKA_TOPIC,
+            messages: [
+                { key: process.env.KAFKA_KEY, value: JSON.stringify(request) },
+            ]
         })
-        await producer.connect()
 
-        let request = geoBoundsToGeoJson(bounds)
-
-        try {
-            await producer.send({
-                topic: process.env.KAFKA_TOPIC,
-                messages: [
-                    { key: process.env.KAFKA_KEY, value: JSON.stringify(request) },
-                ]
-            })
-
-            await producer.disconnect()
-        } catch (e) {
-            await producer.disconnect()
-            throw e
-        }
+        await producer.disconnect()
+    } catch (e) {
+        await producer.disconnect()
+        throw e
     }
+}
