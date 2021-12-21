675e9a074149bca6ca015e09c37cc1c96c45e55b
Lomzie <lomztein2@gmail.com>
2021-12-21 19:58:34 +0100

    can't break promises (i hope)

--- src/kafka.ts
+++ src/kafka.ts
@@ -13,148 +13,150 @@ const hb = hbase({
 })
 
 export async function getResult(bounds: GeoBounds) {
-    try {
-        const tname = process.env.HBASE_TABLE
-        const table = hb.table(tname)
-        var geojson = null
+    const tname = process.env.HBASE_TABLE
+    const table = hb.table(tname)
 
-        table.exists((error: any, success: boolean) => {
-            if (success) {
-                table.scan({
-                    maxVersions: 1, // Might need to change if we want to query back in time.
-                    filter: filter(bounds, 0, Date.now())
-                }, (err: any, rows: any) => {
-                    if (err !== null) {
-                        // tslint:disable-next-line:no-console
-                        console.log(err)
-                    } else {
-                        // tslint:disable-next-line:no-console
-                        console.log(rows)
+    var result = await new Promise(resolve => {
 
-                        var shapes = []
-                        for (let i: number; i < rows.length; i++) {
-                            let row = rows[i]
-                            if (row['column'] === 'shape.geojson') {
-                                shapes.push(JSON.parse(row['$']))
+        try {
+            table.exists((error: any, success: boolean) => {
+                if (success) {
+                    table.scan({
+                        maxVersions: 1, // Might need to change if we want to query back in time.
+                        filter: filter(bounds, 0, Date.now())
+                    }, (err: any, rows: any) => {
+                        if (err !== null) {
+                            // tslint:disable-next-line:no-console
+                            console.log(err)
+                        } else {
+                            // tslint:disable-next-line:no-console
+                            console.log(rows)
+
+                            var shapes = []
+                            for (let i: number; i < rows.length; i++) {
+                                let row = rows[i]
+                                if (row['column'] === 'shape.geojson') {
+                                    shapes.push(JSON.parse(row['$']))
+                                }
+                            }
+                            if (shapes.length > 0) {
+                                resolve(featureCollection(shapes))
+                            } else {
+                                resolve(null)
                             }
                         }
-                        geojson = featureCollection(shapes)
+                    })
+                } else {
+                    // tslint:disable-next-line:no-console
+                    console.log(error)
+                }
+                resolve (null)
+            })
+        } catch (error) {
+            // tslint:disable-next-line:no-console
+            console.log(error)
+        }
+    })
 
-                        if (shapes.length > 0) {
-                            return geojson
-                        } else {
-                            return null
-                        }
-                    }
-                })
-            } else {
-                // tslint:disable-next-line:no-console
-                console.log(error)
-            }
-            return null
-        })
-    } catch (error) {
-        // tslint:disable-next-line:no-console
-        console.log(error)
-    }
+    return result
 }
 
 function polygon(coordinates) {
-    return {
-        "type": "Feature",
-        "geometry": {
-            "type": "Polygon",
-            "coordinates": [
-                coordinates
-            ]
-        },
-    }
-}
+            return {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Polygon",
+                    "coordinates": [
+                        coordinates
+                    ]
+                },
+            }
+        }
 
 function featureCollection(features) {
-    return {
-        "type": "FeatureCollection",
-        "features": features
-    }
-}
+            return {
+                "type": "FeatureCollection",
+                "features": features
+            }
+        }
 
 function filter(bounds: GeoBounds, timestampMin: number, timestampMax: number) {
-    return {
-        'op': 'MUST_PASS_ALL', 'type': 'FilterList', 'filters': [
-            latMinFilter(bounds.minLat),
-            latMaxFilter(bounds.maxLat),
-            lonMinFilter(bounds.minLon),
-            lonMaxFilter(bounds.maxLon),
-            //timestampMinFilter(timestampMin),
-            //timestampMaxFilter(timestampMax) // Unsure how to implement
-        ]
-    }
-}
+            return {
+                'op': 'MUST_PASS_ALL', 'type': 'FilterList', 'filters': [
+                    latMinFilter(bounds.minLat),
+                    latMaxFilter(bounds.maxLat),
+                    lonMinFilter(bounds.minLon),
+                    lonMaxFilter(bounds.maxLon),
+                    //timestampMinFilter(timestampMin),
+                    //timestampMaxFilter(timestampMax) // Unsure how to implement
+                ]
+            }
+        }
 
 // This could be generified a lot but shut up. Might actually do it if it doesn't quickly work out and I have to tweak a bunch.
 function latMinFilter(latMin: number) {
-    return { 'op': 'GREATER', 'type': 'SingleColumnValueFilter', 'family': 'shape', 'qualifier': 'lat', 'comparator': { 'value': latMin.toString(), 'type': 'BinaryComparator' }, 'filterIfColumnMissing': 'true' }
-}
+            return { 'op': 'GREATER', 'type': 'SingleColumnValueFilter', 'family': 'shape', 'qualifier': 'lat', 'comparator': { 'value': latMin.toString(), 'type': 'BinaryComparator' }, 'filterIfColumnMissing': 'true' }
+        }
 
 function latMaxFilter(latMax: number) {
-    return { 'op': 'LESS', 'type': 'SingleColumnValueFilter', 'family': 'shape', 'qualifier': 'lat', 'comparator': { 'value': latMax.toString(), 'type': 'BinaryComparator' }, 'filterIfColumnMissing': 'true' }
-}
+            return { 'op': 'LESS', 'type': 'SingleColumnValueFilter', 'family': 'shape', 'qualifier': 'lat', 'comparator': { 'value': latMax.toString(), 'type': 'BinaryComparator' }, 'filterIfColumnMissing': 'true' }
+        }
 
 function lonMinFilter(lonMin: number) {
-    return { 'op': 'GREATER', 'type': 'SingleColumnValueFilter', 'family': 'shape', 'qualifier': 'long', 'comparator': { 'value': lonMin.toString(), 'type': 'BinaryComparator' }, 'filterIfColumnMissing': 'true' }
-}
+            return { 'op': 'GREATER', 'type': 'SingleColumnValueFilter', 'family': 'shape', 'qualifier': 'long', 'comparator': { 'value': lonMin.toString(), 'type': 'BinaryComparator' }, 'filterIfColumnMissing': 'true' }
+        }
 
 function lonMaxFilter(lonMax: number) {
-    return { 'op': 'LESS', 'type': 'SingleColumnValueFilter', 'family': 'shape', 'qualifier': 'long', 'comparator': { 'value': lonMax.toString(), 'type': 'BinaryComparator' }, 'filterIfColumnMissing': 'true' }
-}
+            return { 'op': 'LESS', 'type': 'SingleColumnValueFilter', 'family': 'shape', 'qualifier': 'long', 'comparator': { 'value': lonMax.toString(), 'type': 'BinaryComparator' }, 'filterIfColumnMissing': 'true' }
+        }
 
 function timestampMinFilter(min: number) {
-    return {}
-}
+            return {}
+        }
 
 function timestampMaxFilter(max: number) {
-    return {}
-}
+            return {}
+        }
 
 function geoBoundsToGeoJson(bounds: GeoBounds) {
-    const geojson = {
-        "type": "FeatureCollection",
-        "features": [
-            {
-                "type": "Feature",
-                "properties": {},
-                "geometry": {
-                    "type": "Polygon",
-                    "coordinates": [[[bounds.minLon, bounds.maxLat], [bounds.maxLon, bounds.maxLat], [bounds.maxLon, bounds.minLat], [bounds.minLon, bounds.minLat], [bounds.minLon, bounds.maxLat]]]
-                }
-            }]
-    }
-    return geojson
-}
+            const geojson = {
+                "type": "FeatureCollection",
+                "features": [
+                    {
+                        "type": "Feature",
+                        "properties": {},
+                        "geometry": {
+                            "type": "Polygon",
+                            "coordinates": [[[bounds.minLon, bounds.maxLat], [bounds.maxLon, bounds.maxLat], [bounds.maxLon, bounds.minLat], [bounds.minLon, bounds.minLat], [bounds.minLon, bounds.maxLat]]]
+                        }
+                    }]
+            }
+            return geojson
+        }
 
 // Returns request UUID.
 export async function sendRequest(bounds: GeoBounds) {
 
-    const producer = kafka.producer({
-        maxInFlightRequests: 1,
-        idempotent: true,
-        transactionalId: process.env.KAFKA_PRODUCER_ID
-    })
-    await producer.connect()
-
-    let request = geoBoundsToGeoJson(bounds)
-
-    try {
-        await producer.send({
-            topic: process.env.KAFKA_TOPIC,
-            messages: [
-                { key: process.env.KAFKA_KEY, value: JSON.stringify(request) },
-            ]
+        const producer = kafka.producer({
+            maxInFlightRequests: 1,
+            idempotent: true,
+            transactionalId: process.env.KAFKA_PRODUCER_ID
         })
+        await producer.connect()
 
-        await producer.disconnect()
-    } catch (e) {
-        await producer.disconnect()
-        throw e
+        let request = geoBoundsToGeoJson(bounds)
+
+        try {
+            await producer.send({
+                topic: process.env.KAFKA_TOPIC,
+                messages: [
+                    { key: process.env.KAFKA_KEY, value: JSON.stringify(request) },
+                ]
+            })
+
+            await producer.disconnect()
+        } catch (e) {
+            await producer.disconnect()
+            throw e
+        }
     }
-}
